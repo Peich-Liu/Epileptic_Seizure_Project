9edb4bd150256769bb2b000417719ddde8932316
Liu Peichen <pliu@eslsrv12.intranet.epfl.ch>
2023-11-12 09:49:48 +0100

    standardize folder

--- file_trans.py
+++ file_trans.py
@@ -1,6 +1,6 @@
 import pandas as pd
 # 读取Parquet文件
-df = pd.read_parquet('/home/pliu/git_repo/10_datasets/SIENA_multi_Features_/PN00/PN00-2-Network.parquet.gzip')
+df = pd.read_parquet('/home/pliu/git_repo/10_datasets/SIENA_Features/PN00/PN00-1-DMe.parquet.gzip')
 
 # 将DataFrame写入CSV文件
-df.to_csv('net00.csv', index=False)
+df.to_csv('net.csv', index=False)
